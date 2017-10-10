0d05b0471d0b516d918f90d75d64c45251221c29
mxydls <15234255611@163.com>
2017-10-10 11:52:57 +0800

    Update ReferencedPtr.cpp

--- ReferencedPtr.cpp
+++ ReferencedPtr.cpp
@@ -51,9 +51,16 @@ public:
 			count_ = r.count_;
 		}
 	}
+	//move constructor
+	ReferencedPtr(ReferencedPtr&& r) nothrow {
+		instance_ = r.instance_;
+		count_ = r.count_;
+		r.instance_ = NULL;
+		r.count_ = NULL; 
+	}
 	~ReferencedPtr() {
 		cout<<"~ReferencedPtr()"<<endl;
-		release();
+		reset();
 	}
 	
 	T* get() {
@@ -66,13 +73,21 @@ public:
 	}
 	
 	ReferencedPtr& operator = (ReferencedPtr & r) {
-		release();
+		reset();
 		if(r.instance_) {
 			(*r.count_)++;
 			count_ = r.count_;
 			instance_ = r.instance_;
 		}
 		return *this;
+	}
+	
+	bool operator == (const ReferencedPtr& r) const {
+		return instance_ == r.instance_;
+	}
+	
+	explicit operator bool() {
+		return instance_ != NULL;
 	}
 	
 	T * operator -> () {
@@ -82,8 +97,8 @@ public:
 		return *instance_;
 	}
 	
-	//we don't have nullptr, so we must use a release function explicitly
-	void release() {
+	//we don't have nullptr, so we must use a reset function explicitly
+	void reset() {
 		if(!instance_) return;
 		(*count_)--;
 		if(*count_ == 0) {
@@ -99,6 +114,7 @@ private:
 	atomic<uint32> * count_ = NULL;
 };
 
+struct nullptr_t: public ReferencedPtr
 
 typedef ReferencedPtr<A> ptr_A;
 
